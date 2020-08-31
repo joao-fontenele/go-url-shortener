70503ba3d0d62124cc7e7210aeaf3593f5573e71
João Paulo Fontenele <joao.fontenele@linx3.com>
2020-08-31 19:44:32 -0400

    feat(shortener): add model and dao for link entity

---
+++ pkg/shortener/link.go
@@ -0,0 +1,21 @@
+package shortener
+
+import (
+	"context"
+	"time"
+)
+
+// Link holds the attributes related to shortened link urls
+type Link struct {
+	Slug      string
+	URL       string
+	CreatedAt time.Time
+}
+
+// LinkDao represents a contract to access a single datastore
+type LinkDao interface {
+	Find(ctx context.Context, slug string) (*Link, error)
+	Insert(ctx context.Context, l *Link) (*Link, error)
+	Update(ctx context.Context, l *Link) (*Link, error)
+	Delete(ctx context.Context, slug string) (*Link, error)
+}
