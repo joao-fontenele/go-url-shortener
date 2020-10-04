70a661c37df7b96684efcbf33826797596307ce8
João Paulo Fontenele <joaopaulojpfb1@gmail.com>
2020-10-03 21:14:58 -0400

    feat(api/router): add a metrics route

--- pkg/api/router/router.go
+++ pkg/api/router/router.go
@@ -5,6 +5,8 @@ import (
 	"github.com/joao-fontenele/go-url-shortener/pkg/api/handler"
 	"github.com/joao-fontenele/go-url-shortener/pkg/api/middleware"
 	"github.com/joao-fontenele/go-url-shortener/pkg/shortener"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
 )
 
 // New configures routes and it's handlers, and return it
@@ -13,6 +15,12 @@ func New(linkService shortener.LinkService) *router.Router {
 
 	internalHandler := &handler.InternalHandler{}
 	router.GET("/internal/status", middleware.Logger(internalHandler.StatusHandler))
+	router.GET(
+		"/internal/metrics",
+		middleware.Logger(
+			fasthttpadaptor.NewFastHTTPHandler(promhttp.Handler()),
+		),
+	)
 
 	linkHandler := &handler.ShortenerHandler{LinkService: linkService}
 	router.POST("/links", middleware.Logger(linkHandler.NewLink))
